3e1dc295701c1361bbf217d0ca7fc256b481b927
OvidiuBacanu <75947602+OvidiuBacanu@users.noreply.github.com>
2021-03-21 18:42:35 +0200

    Add files via upload

---
+++ connect four/connect four/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace connect_four
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(522, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 67);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player 1 ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(569, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 67);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Turn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(522, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(222, 67);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Player 2";
+            this.label3.Visible = false;
+           
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Connect Four";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ connect four/connect four/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace connect_four
+{
+    public partial class Form1 : Form
+    {
+        private Rectangle[] tabla;
+        private int[,] matrice;
+        private int turn,ct;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.tabla= new Rectangle[7];
+            this.matrice = new int[6, 7];
+            this.turn = 1;
+            this.ct = 0;
+        }
+     
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.Blue, 30, 30, 450, 400);
+            for(int i=0;i<6;i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    if (i == 0)
+                    {
+                        this.tabla[j] = new Rectangle(64 * j + 40, 40, 40, 365);
+                    }
+                    e.Graphics.FillEllipse(Brushes.White, 64 * j + 40, 64 * i + 40, 40, 40);
+                }
+            }
+        }
+
+        private int nrcoloana(Point mouse)
+        {
+            for(int i=0;i<this.tabla.Length;i++)
+            {
+                if(mouse.X>=this.tabla[i].X && mouse.Y>=this.tabla[i].Y)
+                {
+                    if(mouse.X<this.tabla[i].X+this.tabla[i].Width && mouse.Y < this.tabla[i].Y + this.tabla[i].Height)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        private int nrrand(int coloana)
+        {
+            for(int i=5;i>=0;i--)
+            {
+                if(this.matrice[i,coloana]==0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int coloana = this.nrcoloana(e.Location);
+            if(coloana!=-1)
+            {
+                int rand = nrrand(coloana);
+                if(rand!=-1)
+                {
+                    this.matrice[rand, coloana] = this.turn;
+                    if(this.turn==1)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        g.FillEllipse(Brushes.Red, 64 * coloana + 40, 64 * rand + 40, 40, 40);
+                    }
+                    if(this.turn == 2)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        g.FillEllipse(Brushes.Gold, 64 * coloana + 40, 64 * rand + 40, 40, 40);
+                    }
+                    int winner = castigator(this.turn);
+                    ct++;
+                    if(winner!=-1)
+                    {
+                        MessageBox.Show("PLAYER " + winner.ToString() + " WINS!");
+                        Application.Restart();
+                    }
+                    if(ct==42)
+                    {
+                        MessageBox.Show("IT'S A TIE!");
+                        Application.Restart();
+                    }
+                    if (this.turn == 1)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        g.FillEllipse(Brushes.Gold, 600, 300, 70, 70);
+                        label1.Visible = false;
+                        label3.Visible = true;
+                        this.turn = 2;
+                    }
+                    else
+                    {
+                        Graphics g = this.CreateGraphics();
+                        g.FillEllipse(Brushes.Red, 600, 300, 70, 70);
+                        label1.Visible = true;
+                        label3.Visible = false;
+                        this.turn = 1;
+                    }
+                        
+                }
+            }
+        }
+        private bool egalitate(int nr1, int nr2, int nr3, int nr4)
+        {
+            if (nr1 == nr2 && nr2 == nr3 && nr3 == nr4 && nr1!=0)
+                return true;
+            else
+                return false;
+        }
+
+        private int castigator(int player)
+        {
+            //vertical
+            for(int i=0;i<3;i++)
+            {
+                for(int j=0;j<7;j++)
+                {
+                    if (egalitate(matrice[i, j], matrice[i + 1, j], matrice[i + 2, j], matrice[i + 3, j]) == true)
+                        return player;
+                }
+            }
+            
+            //orizontal
+            for(int i=0;i<6;i++)
+            {
+                for(int j=0;j<4;j++)
+                {
+                    if (egalitate(matrice[i, j], matrice[i , j+1], matrice[i , j+2], matrice[i , j+3]) == true)
+                        return player;
+                }
+            }
+
+            //diagonala spre dreapta( / )
+            for (int i = 0; i<3; i++)
+            {
+                for (int j = 3; j <7 ; j++)
+                {
+                    if (egalitate(matrice[i, j], matrice[i+1, j - 1], matrice[i+2, j - 2], matrice[i+3, j - 3]) == true)
+                    {
+                        return player;
+                    }
+                }
+            }
+
+            //diagonala spre stanga( \ )
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (egalitate(matrice[i, j], matrice[i + 1, j + 1], matrice[i + 2, j + 2], matrice[i + 3, j + 3]) == true)
+                        return player;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
